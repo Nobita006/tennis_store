7e1ffa18a93e21b40bd396802150b241a8078202
Sayan Das <sayandas001s@gmail.com>
2024-08-23 17:24:17 +0530

    products page working

--- store/models.py
+++ store/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     
     def __str__(self):
         return self.name
@@ -16,3 +16,8 @@ class Cart(models.Model):
 
     def __str__(self):
         return f'Cart of {self.user.username}'
+
+class CartItem(models.Model): # maybe maybe
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.core.paginator import Paginator
 from .models import Product, Cart
 
 def home(request):
@@ -18,9 +19,22 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 def product_list(request):
+    query = request.GET.get('q') or ''
     products = Product.objects.all()
-    return render(request, 'products.html', {'products': products})
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    paginator = Paginator(products, 10)  # Show 10 products per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'products.html', {'page_obj': page_obj, 'query': query})
 
 def view_cart(request):
     cart = Cart.objects.get(user=request.user)
     return render(request, 'cart.html', {'cart': cart})
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login')
