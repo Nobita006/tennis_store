48fa2a68a3d5608854bc31de2c3c7bd7dfe4d776
Sayan Das <sayandas001s@gmail.com>
2024-08-23 17:42:02 +0530

    Fixed login logout issue

--- tennis_store/settings.py
+++ tennis_store/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_REDIRECT_URL = '/products/'
+
+LOGOUT_REDIRECT_URL = '/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
