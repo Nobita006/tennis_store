6d6c12f46f2291c6c47845549272cb4d11ef1cc9
New Name <sayandas001s@gmail.com>
2024-08-29 10:59:58 +0530

    added readme requirment and venv

--- tennis_store/settings.py
+++ tennis_store/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-t%z3b8+r*-n@rcj+84&_#8h!c6wep%_dug26yg-45b_%xj^fs+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
